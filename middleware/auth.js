7fee7b2fd86daafea92741c662ba11bf775f0f0b
Mugilan <mugilancodes@gmail.com>
2020-04-28 16:30:40 +0530

    Register Patient - Refactored 4

---
+++ middleware/auth.js
@@ -0,0 +1,32 @@
+const cassandra = require('cassandra-driver');
+
+const client = new cassandra.Client({
+  contactPoints: ['localhost'],
+  localDataCenter: 'datacenter1',
+  keyspace: 'healthcare',
+});
+client.connect();
+
+module.exports = async (req, res, next) => {
+  const token = req.header('x-auth-token');
+  if (!token) {
+    return res.status(401).json({ msg: 'No Token, Authorization denied' });
+  }
+
+  try {
+    const decoded = (
+      await client.execute(
+        'SELECT * FROM patient WHERE id = ? ALLOW FILTERING',
+        [token],
+        {
+          prepare: true,
+        }
+      )
+    ).rows[0];
+    // console.log(`decoded = ${JSON.stringify(decoded)}`);
+    req.email = decoded.email;
+    next();
+  } catch (err) {
+    res.status(401).json({ msg: 'Token is Invalid' });
+  }
+};
--- routes/api/health.js
+++ routes/api/health.js
@@ -11,10 +11,13 @@ const client = new cassandra.Client({
 });
 client.connect();
 
+const admin = require('../../middleware/admin');
+const auth = require('../../middleware/auth');
+
 // @route   GET api/health
 // @desc    List all Table Details
 // @access  Public
-router.get('/', async (req, res) => {
+router.get('/', admin, async (req, res) => {
   try {
     const listAdmins = (await client.execute('SELECT * FROM admin')).rows;
     const listPatients = (await client.execute('SELECT * FROM patient')).rows;
@@ -57,32 +60,47 @@ router.post(
         typeof getPatientDetail === 'undefined' ||
         getPatientDetail.email !== email;
 
-      if (isPatientNotExist) {
-        const registerQuery =
-          'INSERT INTO patient ( id, name, email, addr, dob, gender, phno, pwd ) VALUES ( ?, ?, ?, ?, ?, ?, ?, ? ) ;';
-        const params = [id, name, email, addr, dob, gender, phno, pwd];
-        await client.execute(registerQuery, params, { prepare: true });
-
-        const returnEmail = (
-          await client.execute(
-            'SELECT * FROM patient WHERE email = ?',
-            [email],
-            {
-              prepare: true,
-            }
-          )
-        ).rows[0].email;
-
-        return res.json(returnEmail);
+      if (!isPatientNotExist) {
+        return res.status(400).json({ errors: [{ msg: 'Patient Exists' }] });
       }
 
-      res.status(400).json({ errors: [{ msg: 'Patient Exists' }] });
+      const registerQuery =
+        'INSERT INTO patient ( id, name, email, addr, dob, gender, phno, pwd ) VALUES ( ?, ?, ?, ?, ?, ?, ?, ? ) ;';
+      const params = [id, name, email, addr, dob, gender, phno, pwd];
+      await client.execute(registerQuery, params, { prepare: true });
+
+      const returnId = (
+        await client.execute('SELECT * FROM patient WHERE email = ?', [email], {
+          prepare: true,
+        })
+      ).rows[0].id;
+
+      res.json({ token: returnId });
     } catch (err) {
       console.error(err.message);
       res.status(500).send('Server Error');
     }
   }
 );
+
+// @route   POST api/health/patient
+// @desc    Get Auth Patient
+// @access  Private
+router.get('/patient', auth, async (req, res) => {
+  try {
+    const patient = (
+      await client.execute(
+        'SELECT * FROM patient WHERE email = ?',
+        [req.email],
+        { prepare: true }
+      )
+    ).rows[0];
+    res.json(patient);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
 
 // @route   POST api/health/patient/login
 // @desc    Login Patient
