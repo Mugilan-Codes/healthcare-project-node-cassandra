84af61ac8284ae29ef9ba68047a87321847a7fc0
Mugilan <mugilancodes@gmail.com>
2020-04-28 12:53:45 +0530

    Register Patient - Refactored 1

--- routes/api/health.js
+++ routes/api/health.js
@@ -11,15 +11,13 @@ const client = new cassandra.Client({
 client.connect();
 
 // @route   GET api/health
-// @desc    Test Route
+// @desc    List all Table Details
 // @access  Public
 router.get('/', async (req, res) => {
   try {
-    const listAdmins = await (await client.execute('SELECT * FROM admin')).rows;
-    const listPatients = await (await client.execute('SELECT * FROM patient'))
-      .rows;
-    const listDoctors = await (await client.execute('SELECT * FROM doctor'))
-      .rows;
+    const listAdmins = (await client.execute('SELECT * FROM admin')).rows;
+    const listPatients = (await client.execute('SELECT * FROM patient')).rows;
+    const listDoctors = (await client.execute('SELECT * FROM doctor')).rows;
     console.log(listAdmins);
     console.log(listPatients);
     console.log(listDoctors);
@@ -30,40 +28,62 @@ router.get('/', async (req, res) => {
   }
 });
 
-// @route   POST api/health/register
+// @route   POST api/health/patient/register
 // @desc    Register Patient
 // @access  Public
-router.post('/register', async (req, res) => {
-  const { email, addr, dob, gender, name, phno, pwd } = req.body;
+router.post('/patient/register', async (req, res) => {
+  const { name, email, addr, dob, gender, phno, pwd } = req.body;
   try {
     const id = uuid.random();
-    const registerQueries = [
-      {
-        query:
-          'INSERT INTO patient_sign_up ( name, gender, dob, addr, email, phno, pwd ) VALUES ( ?, ?, ?, ?, ?, ?, ? ) ;',
-        params: [name, gender, dob, addr, email, phno, pwd],
-      },
-      {
-        query: 'INSERT INTO patient_sign_in ( email, pwd ) VALUES ( ?, ? ) ;',
-        params: [email, pwd],
-      },
-      {
-        query: 'INSERT INTO patient_id ( name, id ) VALUES ( ?, ? ) ;',
-        params: [name, id],
-      },
-    ];
-    await client.batch(registerQueries, { prepare: true });
-    res.send('Patient Registered');
+
+    // const registerQueries = [
+    //   {
+    //     query:
+    //       'INSERT INTO patient_sign_up ( name, gender, dob, addr, email, phno, pwd ) VALUES ( ?, ?, ?, ?, ?, ?, ? ) ;',
+    //     params: [name, gender, dob, addr, email, phno, pwd],
+    //   },
+    //   {
+    //     query: 'INSERT INTO patient_sign_in ( email, pwd ) VALUES ( ?, ? ) ;',
+    //     params: [email, pwd],
+    //   },
+    //   {
+    //     query: 'INSERT INTO patient_id ( name, id ) VALUES ( ?, ? ) ;',
+    //     params: [name, id],
+    //   },
+    // ];
+    // await client.batch(registerQueries, { prepare: true });
+    const checkPatientExists = (
+      await client.execute('SELECT * FROM patient WHERE email = ?', [email], {
+        prepare: true,
+      })
+    ).rows[0];
+    const isPatientNotExist =
+      typeof checkPatientExists === 'undefined' ||
+      checkPatientExists.email !== email;
+    console.log(isPatientNotExist);
+
+    if (isPatientNotExist) {
+      const registerQuery =
+        'INSERT INTO patient ( id, name, email, addr, dob, gender, phno, pwd ) VALUES ( ?, ?, ?, ?, ?, ?, ?, ? ) ;';
+      const params = [id, name, email, addr, dob, gender, phno, pwd];
+      const registerPatient = await client.execute(registerQuery, params, {
+        prepare: true,
+      });
+
+      return res.json(registerPatient);
+    }
+
+    res.json(checkPatientExists);
   } catch (err) {
     console.error(err.message);
     res.status(500).send('Server Error');
   }
 });
 
-// @route   POST api/health/login
+// @route   POST api/health/patient/login
 // @desc    Login Patient
 // @access  Public
-router.post('/login', async (req, res) => {
+router.post('/patient/login', async (req, res) => {
   const { email, pwd } = req.body;
   try {
     const query = `SELECT * FROM patient_sign_in WHERE email = ?`;
