31e36e485430ab7d434b963baac5af2f985d9107
Mugilan <mugilancodes@gmail.com>
2020-05-03 10:56:19 +0530

    Create Redux Store

--- client/src/App.js
+++ client/src/App.js
@@ -10,25 +10,31 @@ import DoctorLogin from './components/auth/DoctorLogin';
 import DoctorRegister from './components/auth/DoctorRegister';
 // import Footer from './components/layout/Footer';
 
+// Redux
+import { Provider } from 'react-redux';
+import store from './store';
+
 import './App.css';
 
 const App = () => (
-  <Router>
-    <Fragment>
-      <Navbar />
-      <Route exact path='/' component={Landing} />
-      <Route exact path='/doctor' component={DoctorLanding} />
-      <section className='container'>
-        <Switch>
-          <Route exact path='/register' component={Register} />
-          <Route exact path='/login' component={Login} />
-          <Route exact path='/doctor-register' component={DoctorRegister} />
-          <Route exact path='/doctor-login' component={DoctorLogin} />
-        </Switch>
-      </section>
-      {/* <Footer /> */}
-    </Fragment>
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Route exact path='/' component={Landing} />
+        <Route exact path='/doctor' component={DoctorLanding} />
+        <section className='container'>
+          <Switch>
+            <Route exact path='/register' component={Register} />
+            <Route exact path='/login' component={Login} />
+            <Route exact path='/doctor-register' component={DoctorRegister} />
+            <Route exact path='/doctor-login' component={DoctorLogin} />
+          </Switch>
+        </section>
+        {/* <Footer /> */}
+      </Fragment>
+    </Router>
+  </Provider>
 );
 
 export default App;
