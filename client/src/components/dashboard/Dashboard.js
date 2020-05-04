b084542708a094263174be5c35e48fd2ddaa6e9a
Mugilan <mugilancodes@gmail.com>
2020-05-04 12:19:15 +0530

    Current Patient Card in Dashboard

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -8,7 +8,57 @@ const Dashboard = ({ auth: { user, loading } }) => {
   return loading && user === null ? (
     <Spinner />
   ) : (
-    <Fragment>Patient Dashboard</Fragment>
+    <Fragment>
+      <h1 className='large text-primary'>Dashboard</h1>
+      <p className='lead'>
+        <i className='las la-hospital'></i>Welcome, {user.patient.name}
+      </p>
+
+      <div className='user-profile bg-light'>
+        <img
+          className='round-img'
+          src={`https://ui-avatars.com/api/?name=${user.patient.name
+            .replace(/\s\s+/g, ' ')
+            .split('')
+            .join('+')}&size=200`}
+          alt='img'
+        />
+
+        <div>
+          <h2>
+            Name: <span>{user.patient.name}</span>
+          </h2>
+          <p>
+            Email: <span>{user.patient.email}</span>
+          </p>
+          <p>
+            Phone:{' '}
+            <span>
+              {user.patient.phno !== null ? user.patient.phno : '--Nil--'}
+            </span>
+          </p>
+        </div>
+
+        <div>
+          <p>
+            Address:{' '}
+            <span>
+              {user.patient.addr !== null
+                ? user.patient.addr.length < 20
+                  ? user.patient.addr
+                  : 'Too Long to Display'
+                : '--Nil--'}
+            </span>
+          </p>
+          <p>
+            DOB: <span>{user.patient.dob}</span>
+          </p>
+          <p>
+            Gender: <span>{user.patient.gender}</span>
+          </p>
+        </div>
+      </div>
+    </Fragment>
   );
 };
 
