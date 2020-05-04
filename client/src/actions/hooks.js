88e10d8bfc033c5b6f67e58f092ee3df52df1bb6
Mugilan <mugilancodes@gmail.com>
2020-05-04 09:51:04 +0530

    Remove hooks.js warning

--- client/src/actions/hooks.js
+++ client/src/actions/hooks.js
@@ -3,15 +3,15 @@ import { useState, useEffect } from 'react';
 const useFetch = (url) => {
   const [data, setData] = useState([]);
 
-  const fetchUrl = async () => {
-    const response = await fetch(url);
-    const json = await response.json();
-    setData(json);
-  };
-
   useEffect(() => {
+    const fetchUrl = async () => {
+      const response = await fetch(url);
+      const json = await response.json();
+      setData(json);
+    };
+
     fetchUrl();
-  }, []);
+  }, [url]);
 
   return [data];
 };
