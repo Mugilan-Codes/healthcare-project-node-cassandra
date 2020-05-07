2b190fd6f34820db32f8e636e15beb8e6cc2bc3e
Mugilan <mugilancodes@gmail.com>
2020-05-07 12:01:29 +0530

    News Api Refined

--- client/src/components/layout/News.js
+++ client/src/components/layout/News.js
@@ -3,27 +3,32 @@ import axios from 'axios';
 import Moment from 'react-moment';
 
 const News = () => {
+  const reloadPage = () => window.location.reload(false);
+
   const [data, setData] = useState([]);
 
+  if (localStorage.token) {
+    localStorage.removeItem('token');
+  }
+
   useEffect(() => {
+    const apiCall = async () => {
+      try {
+        //todo Put the API Key in Separate file
+
+        const res = await axios.get(
+          'http://newsapi.org/v2/top-headlines?country=in&category=health&apiKey=4a2855e432f147e5b42b39bc96011a68'
+        );
+
+        setData(res.data.articles);
+      } catch (err) {
+        reloadPage();
+        console.error(err.message);
+      }
+    };
+
     apiCall();
   }, []);
-
-  const apiCall = async () => {
-    try {
-      //todo Put the API Key in Separate file
-
-      const res = await axios.get(
-        'http://newsapi.org/v2/top-headlines?country=in&category=health&apiKey=4a2855e432f147e5b42b39bc96011a68'
-      );
-
-      setData(res.data.articles);
-    } catch (err) {
-      alert('Error in Api Call ' + err.message);
-    }
-  };
-
-  console.log(data);
 
   const articles = data.map((article, i) => (
     <div key={i} className='news-profile bg-light'>
@@ -47,7 +52,12 @@ const News = () => {
     </div>
   ));
 
-  return <Fragment>{articles}</Fragment>;
+  return (
+    <Fragment>
+      <h1 className='text-primary my-1'>Latest Healthcare News in India</h1>
+      {articles}
+    </Fragment>
+  );
 };
 
 export default News;
